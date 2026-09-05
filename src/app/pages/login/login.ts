2c53d2e3090678b70e2e0eabbc2937ae0f17b481
Kamil Mazur <kamilosmaz@gmail.com>
2026-09-05 19:37:48 +0200

    Zbudowanie funkcji loginu, połączenie loginu z aws lambda

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   selector: 'app-login',
   styleUrl: './login.css',
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login {
+  password = '';
+  log = '';
+
+  constructor(private router: Router){}
+
+  goHome(){
+    this.router.navigate(['/mainPage']);
+  }
+
+  async login(){
+    const response = await fetch("",{
+      method: 'POST',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({
+        log : this.log,
+        password : this.password
+      })
+    });
+
+    if (response.ok){
+      this.goHome();
+    }else{
+      console.error("Błąd logowania")
+    }
+  }
+}

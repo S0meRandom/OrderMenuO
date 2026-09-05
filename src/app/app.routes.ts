4a56abb18cc4c98f32aeed50c023aaecb3ff1244
Kamil Mazur <kamilosmaz@gmail.com>
2026-09-05 15:48:40 +0200

    Utworzenie części wizualnej do ekranu logowania

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Login } from './pages/login/login';
 import { MainPage } from "./pages/main-page/main-page"
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: Login},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: '**', redirectTo: 'login' }
 ];

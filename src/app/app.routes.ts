98d7f2de0bf5e6074fbc83fd170e5ec85ec217c9
Kamil Mazur <kamilosmaz@gmail.com>
2026-09-05 15:13:00 +0200

    Initial commit(login i mainPage)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { MainPage } from "./pages/main-page/main-page"
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: Login},
+  { path: '**', redirectTo: 'login' }
+];
---
+++ src/app/pages/main-page/main-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-main-page',
+  styleUrl: './main-page.css',
+  templateUrl: './main-page.html',
+})
+export class MainPage {}

17e09301073946ad15c645dc6841e661f8544e84
Kamil Mazur <kamilosmaz@gmail.com>
2026-09-06 22:21:20 +0200

    Wprowadzenie formularza dodawania zamówienia i dodanie podziału na strone "Zamówienia" "Klienci na cito"

--- src/app/pages/main-page/main-page.ts
+++ src/app/pages/main-page/main-page.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
 
+export interface Order {
+  id : number,
+  name : String,
+  order : String,
+  status : String,
+  quantity : number,
+  price : number,
+  data: String,
+  country: String
+}
+
 @Component({
   imports: [],
   selector: 'app-main-page',
   styleUrl: './main-page.css',
   templateUrl: './main-page.html',
 })
-export class MainPage {}
+export class MainPage {
+  modalOpen = false;
+  citoOrdersPage = false;
+  mainOrdersPage = true;
+
+  citoOrders : Order[] = [
+    {id: 2, name: 'Iwona Staszewska', order: 'Chmuroskos', status: "Aktywne",quantity: 2, price: 150, data: '2026-09-01', country: 'Polska' }
+  ];
+
+  orders: Order[] = [
+    {id: 1, name: 'Jan Kowalski', order: 'Szczerbatek', status: "Aktywne",quantity: 2, price: 150, data: '2026-09-01', country: 'Polska' },
+  ];
+
+
+}

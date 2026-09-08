62f88b293d9a3ef3696bb9a3e3c4865bcf246c6a
Kamil Mazur <kamilosmaz@gmail.com>
2026-09-08 23:53:14 +0200

    Wprowadzenie możliwości dodawania zamówień i odczytu zamówień z aws DynamoDB

--- src/app/pages/main-page/main-page.ts
+++ src/app/pages/main-page/main-page.ts
@@ -1,34 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {environment} from '../../../environments/environment.development';
 
 export interface Order {
-  id : number,
-  name : String,
-  order : String,
-  status : String,
+  id: string,
+  name : string,
+  order : string,
+  status : string,
   quantity : number,
   price : number,
-  data: String,
-  country: String
+  date: string,
+  country: string,
+  cito: boolean
 }
 
 @Component({
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   selector: 'app-main-page',
   styleUrl: './main-page.css',
   templateUrl: './main-page.html',
 })
-export class MainPage {
+export class MainPage implements OnInit{
   modalOpen = false;
   citoOrdersPage = false;
   mainOrdersPage = true;
+  newOrderClientName = '';
+  newOrderOrderName = '';
+  newOrderQuantity = 0;
+  newOrderPrice = 0;
+  newOrderCountry = '';
+  newOrderCito = false;
+  orders : Order[] = [];
+  citoOrders = this.getCitoOrders();
 
-  citoOrders : Order[] = [
-    {id: 2, name: 'Iwona Staszewska', order: 'Chmuroskos', status: "Aktywne",quantity: 2, price: 150, data: '2026-09-01', country: 'Polska' }
-  ];
+  ngOnInit() {
+    this.fetchOrders();
+  }
+  async fetchOrders() {
+    try {
+      const response = await fetch(environment.apiUrl, {
+        method: 'GET',
+        credentials: 'include'
+      });
 
-  orders: Order[] = [
-    {id: 1, name: 'Jan Kowalski', order: 'Szczerbatek', status: "Aktywne",quantity: 2, price: 150, data: '2026-09-01', country: 'Polska' },
-  ];
+      if (response.ok) {
+        this.orders = await response.json();
+      }
+    } catch (error) {
+      console.error('Błąd podczas pobierania danych:', error);
+    }
+  }
+  getCitoOrders(): Order[] {
+    return this.orders.filter(order => order.cito === true);
+  }
+
+  async createNewOrder() {
+    const newOrder = {
+      clientName : this.newOrderClientName,
+      orderName : this.newOrderOrderName,
+      orderQuantity : this.newOrderQuantity,
+      orderPrice : this.newOrderPrice,
+      orderCountry : this.newOrderCountry,
+      isOrderCito : this.newOrderCito
+    };
+    try{
+      const response = await fetch(environment.apiUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        body: JSON.stringify(newOrder)
+      });
+      if(response.ok){
+        this.resetForm();
+        this.fetchOrders();
+      }
+    }catch(error){
+
+    }
+  }
+  resetForm() {
+    this.newOrderClientName = '';
+    this.newOrderOrderName = '';
+    this.newOrderCountry = '';
+    this.newOrderCito = false;
+    this.newOrderPrice = 0;
+    this.newOrderQuantity = 0;
+
+  }
+
 
 
 }
